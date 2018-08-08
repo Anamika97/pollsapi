8896db3ad751498469b2e05fe2a404c44b8d22d6
Anamika Awasthi <anamika@voiro.com>
2018-08-08 12:06:59 +0530

    setup for converting polls into a pip package added

--- polls/setup.py
+++ polls/setup.py
@@ -1,20 +1,17 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='pollsapi',
+    name='polls',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='A simple Django app to conduct Web-based polls.',
-    long_description=README,
     author='Anamika',
     author_email='anamika@voiro.com',
     classifiers=[
